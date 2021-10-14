92b8265d5b05e5b4437365c41f21090780243e3c
Enumus <enumus@gmail.com>
2021-10-14 15:05:41 +0200

    added test endpoint

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ app.register_blueprint(api_reporter)
 def home():
     abort(404)
 
+@app.route('/test', methods=['GET'])
+def test():
+    return "This is the test result", 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
